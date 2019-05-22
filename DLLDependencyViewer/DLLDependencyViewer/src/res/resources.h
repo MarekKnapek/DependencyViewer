51e90018b38680e5f6d6c9fce521dcd78c9a3f82
Marek Knápek <knapek.mar@gmail.com>
2019-05-22 01:03:00 +0000

    Add resources. Add tree icons.

---
+++ DLLDependencyViewer/DLLDependencyViewer/src/res/resources.h
@@ -0,0 +1,43 @@
+#pragma once
+
+
+#define s_res_icons_tree 2
+
+
+#define s_res_icon_missing 0
+#define s_res_icon_invalid 1
+#define s_res_icon_normal 2
+#define s_res_icon_duplicate 3
+#define s_res_icon_warning 4
+#define s_res_icon_warning_duplicate 5
+
+#define s_res_icon_normal_64 6
+#define s_res_icon_duplicate_64 7
+#define s_res_icon_warning_64 8
+#define s_res_icon_warning_duplicate_64 9
+
+
+#define s_res_icon_missing_forward 10
+#define s_res_icon_invalid_forward 11
+#define s_res_icon_normal_forward 12
+#define s_res_icon_duplicate_forward 13
+#define s_res_icon_warning_forward 14
+#define s_res_icon_warning_duplicate_forward 15
+
+#define s_res_icon_normal_forward_64 16
+#define s_res_icon_duplicate_forward_64 17
+#define s_res_icon_warning_forward_64 18
+#define s_res_icon_warning_duplicate_forward_64 19
+
+
+#define s_res_icon_missing_delay 20
+#define s_res_icon_invalid_delay 21
+#define s_res_icon_normal_delay 22
+#define s_res_icon_duplicate_delay 23
+#define s_res_icon_warning_delay 24
+#define s_res_icon_warning_duplicate_delay 25
+
+#define s_res_icon_normal_delay_64 26
+#define s_res_icon_duplicate_delay_64 27
+#define s_res_icon_warning_delay_64 28
+#define s_res_icon_warning_duplicate_delay_64 29
