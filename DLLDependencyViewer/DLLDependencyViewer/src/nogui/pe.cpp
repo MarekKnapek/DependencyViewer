b597faaa7a9278b6f30b3d05cd81c2fe48f5f59a
Marek Knápek <knapek.mar@gmail.com>
2019-05-28 01:02:00 +0000

    Some DLLs don't export anything.

--- DLLDependencyViewer/DLLDependencyViewer/src/nogui/pe.cpp
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/pe.cpp
@@ -573,6 +573,10 @@ pe_export_table_info pe_process_export_table(void const* const fd, int const fs,
 	VERIFY(export_dir.m_ordinal_base + export_dir.m_export_address_count <= 0xFFFF);
 	VERIFY(export_dir.m_names_count <= export_dir.m_export_address_count);
 	VERIFY((export_dir.m_export_name_table_rva == 0 && export_dir.m_ordinal_table_rva == 0) || (export_dir.m_export_name_table_rva != 0 && export_dir.m_ordinal_table_rva != 0));
+	if(export_dir.m_export_address_count == 0)
+	{
+		return ret;
+	}
 
 	// 64k export names should be enough for everybody.
 	VERIFY(export_dir.m_names_count <= 64 * 1024);
