1ab6ffde1efac25a6986098176c9d3363a974c36
Marek Knápek <knapek.mar@gmail.com>
2019-08-18 03:08:00 +0000

    Fix file_exists implementation.

--- DLLDependencyViewer/DLLDependencyViewer/src/nogui/file_system.cpp
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/file_system.cpp
@@ -1,10 +1,9 @@
 #include "file_system.h"
 
-#include <windows.h>
+#include <experimental/filesystem>
 
 
 bool file_exists(wchar_t const* const& file_path)
 {
-  DWORD const got_attrib = GetFileAttributesW(file_path);
-	return (got_attrib != INVALID_FILE_ATTRIBUTES) && ((got_attrib & FILE_ATTRIBUTE_DIRECTORY) != 0);
+	return std::experimental::filesystem::exists(file_path);
 }
