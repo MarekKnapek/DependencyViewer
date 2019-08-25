6605c6b9274944fcee5b874457177a520d6a2c17
Marek Knápek <knapek.mar@gmail.com>
2019-08-25 00:01:00 +0000

    smart_library

---
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/smart_library.cpp
@@ -0,0 +1,22 @@
+#include "smart_library.h"
+
+#include <cassert>
+
+
+void library_deleter::operator()(HMODULE const& library) const
+{
+	BOOL const freed = FreeLibrary(library);
+	assert(freed != 0);
+}
+
+smart_library load_library(wchar_t const* const& library_name)
+{
+	return smart_library(LoadLibraryW(library_name));
+}
+
+function_ptr_t get_function_address(smart_library const& library, char const* const& function_name)
+{
+	static_assert(sizeof(function_ptr_t) == sizeof(decltype(GetProcAddress(HMODULE{}, LPCSTR{}))), "");
+	static_assert(alignof(function_ptr_t) == alignof(decltype(GetProcAddress(HMODULE{}, LPCSTR{}))), "");
+	return reinterpret_cast<function_ptr_t>(GetProcAddress(library.get(), function_name));
+}
---
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/smart_library.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include <memory>
+#include <type_traits>
+
+#include <windows.h>
+
+
+struct library_deleter;
+
+
+using HMODULE_value_type = std::remove_reference_t<decltype(*(HMODULE{}))>;
+using function_ptr_t = void(*)();
+using smart_library = std::unique_ptr<HMODULE_value_type, library_deleter>;
+
+
+struct library_deleter
+{
+	void operator()(HMODULE const& library) const;
+};
+
+
+smart_library load_library(wchar_t const* const& library_name);
+function_ptr_t get_function_address(smart_library const& library, char const* const& function_name);
