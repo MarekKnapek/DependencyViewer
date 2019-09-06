754a889e3a237faf02e294eaa401c8caec69e907
Marek Knápek <knapek.mar@gmail.com>
2019-09-06 01:04:00 +0000

    Remove WARN macro.

--- DLLDependencyViewer/DLLDependencyViewer/src/nogui/assert.h
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/assert.h
@@ -4,7 +4,6 @@
 #include <cassert>
 
 
-#define WARN(X) do{ if(!(X)){ assert((assert_function(L"DLLDependencyViewer Warning: " L ## #X L"\x0D\x0A"), true)); } }while(false)
 #define WARN_M(X, M) do{ if(!(X)){ assert((assert_function(L"DLLDependencyViewer Warning: " M L"\x0D\x0A"), true)); } }while(false)
 #define WARN_M_R(X, M, R) do{ if(!(X)){ assert((assert_function(L"DLLDependencyViewer Warning: " M L"\x0D\x0A"), true)); return R; } }while(false)
 
