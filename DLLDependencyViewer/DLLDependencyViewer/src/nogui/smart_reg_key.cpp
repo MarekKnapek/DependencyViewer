081bb23dd9db7ef1811b9400a28d65d853799ff4
Marek Knápek <knapek.mar@gmail.com>
2019-09-12 00:02:00 +0000

    smart_reg_key

---
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/smart_reg_key.cpp
@@ -0,0 +1,17 @@
+#include "smart_reg_key.h"
+
+#include <cassert>
+
+
+void reg_close_key_deleter::operator()(void* const key) const
+{
+	static_assert(sizeof(void*) == sizeof(HKEY), "");
+	LSTATUS const closed = RegCloseKey(reinterpret_cast<HKEY>(key));
+	assert(closed == ERROR_SUCCESS);
+}
+
+smart_reg_key make_smart_reg_key(HKEY const key)
+{
+	static_assert(sizeof(HKEY) == sizeof(void*), "");
+	return smart_reg_key(reinterpret_cast<void*>(key));
+}
---
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/smart_reg_key.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#include <memory>
+
+#include <windows.h>
+
+
+struct reg_close_key_deleter
+{
+public:
+	void operator()(void* const key) const;
+};
+typedef std::unique_ptr<void, reg_close_key_deleter> smart_reg_key;
+
+smart_reg_key make_smart_reg_key(HKEY const key);
