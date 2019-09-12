488836efbf622b290312bc383a6b3357e78d34f6
Marek Knápek <knapek.mar@gmail.com>
2019-09-12 00:06:00 +0000

    get_symbols_from_addresses_task_t

--- DLLDependencyViewer/DLLDependencyViewer/src/nogui/dbg_provider.cpp
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/dbg_provider.cpp
@@ -1,5 +1,7 @@
 #include "dbg_provider.h"
 
+#include "scope_exit.h"
+
 #include <cassert>
 #include <memory>
 
@@ -23,6 +25,11 @@ dbg_provider::dbg_provider():
 dbg_provider::~dbg_provider()
 {
 	add_task(&dbg_provider::deinit_task, nullptr);
+}
+
+void dbg_provider::get_symbols_from_addresses(get_symbols_from_addresses_task_t* const param)
+{
+	add_task(&dbg_provider::get_symbols_from_addresses_task, param);
 }
 
 void dbg_provider::add_task(dbg_provider_task_t const task, dbg_provider_param_t const param)
@@ -68,4 +75,54 @@ void dbg_provider::deinit_task(dbg_provider_param_t const param)
 	assert(cleaned != FALSE);
 	m_dbghelp.m_symsrv_dll.reset();
 	m_dbghelp.m_dbghelp_dll.reset();
+}
+
+void dbg_provider::get_symbols_from_addresses_task(dbg_provider_param_t const param)
+{
+	assert(param);
+	get_symbols_from_addresses_task_t* const task = reinterpret_cast<get_symbols_from_addresses_task_t*>(param);
+	auto const fn_call_callback = mk::make_scope_exit([&]()
+	{
+		auto const callback = task->m_callback_function.exchange(nullptr);
+		(*callback)(task);
+	});
+	if(task->m_canceled.load() == true)
+	{
+		return;
+	}
+	DWORD64 const sym_module = m_dbghelp.m_fn_SymLoadModuleExW(GetCurrentProcess(), nullptr, task->m_module_path->m_str, nullptr, 0, 0, nullptr, 0);
+	if(sym_module == 0)
+	{
+		return;
+	}
+	auto const fn_unload_module = mk::make_scope_exit([&]()
+	{
+		BOOL const unloaded = m_dbghelp.m_fn_SymUnloadModule64(GetCurrentProcess(), sym_module);
+		assert(unloaded != FALSE);
+	});
+	int const n = static_cast<int>(task->m_addresses.size());
+	task->m_symbol_names.resize(n);
+	for(int i = 0; i != n; ++i)
+	{
+		DWORD64 displacement;
+		union symbol_info_t
+		{
+			SYMBOL_INFOW sym_info;
+			char buff[sizeof(SYMBOL_INFOW) + MAX_SYM_NAME * sizeof(wchar_t)];
+		};
+		symbol_info_t symbol_info_v;
+		SYMBOL_INFOW& symbol_info = symbol_info_v.sym_info;
+		symbol_info.SizeOfStruct = sizeof(SYMBOL_INFOW);
+		symbol_info.MaxNameLen = MAX_SYM_NAME;
+		DWORD64 const address64 = sym_module + task->m_addresses[i];
+		BOOL const got_sym = m_dbghelp.m_fn_SymFromAddrW(GetCurrentProcess(), address64, &displacement, &symbol_info);
+		if(got_sym != FALSE)
+		{
+			task->m_symbol_names[i].assign(symbol_info.Name, symbol_info.Name + symbol_info.NameLen);
+		}
+		else
+		{
+			task->m_symbol_names[i].clear();
+		}
+	}
 }
--- DLLDependencyViewer/DLLDependencyViewer/src/nogui/dbg_provider.h
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/dbg_provider.h
@@ -2,7 +2,13 @@
 
 
 #include "dbghelp.h"
+#include "pe.h"
 #include "thread_worker.h"
+#include "unique_strings.h"
+
+#include <atomic>
+#include <string>
+#include <vector>
 
 
 class dbg_provider;
@@ -12,15 +18,30 @@ typedef void* dbg_provider_param_t;
 typedef void(dbg_provider::* dbg_provider_task_t)(dbg_provider_param_t const param);
 
 
+struct get_symbols_from_addresses_task_t
+{
+	typedef void(* callback_function_t)(get_symbols_from_addresses_task_t* const);
+	std::atomic<bool> m_canceled;
+	wstring const* m_module_path;
+	std::vector<std::uint32_t> m_addresses;
+	std::vector<pe_export_address_entry*> m_export_entries;
+	std::vector<std::wstring> m_symbol_names;
+	std::atomic<callback_function_t> m_callback_function;
+	void* m_hwnd;
+};
+
+
 class dbg_provider
 {
 public:
 	dbg_provider();
 	~dbg_provider();
+	void get_symbols_from_addresses(get_symbols_from_addresses_task_t* const param);
 private:
 	void add_task(dbg_provider_task_t const task, dbg_provider_param_t const param);
 	void init_task(dbg_provider_param_t const param);
 	void deinit_task(dbg_provider_param_t const param);
+	void get_symbols_from_addresses_task(dbg_provider_param_t const param);
 private:
 	bool m_sym_inited;
 	dbghelp m_dbghelp;
