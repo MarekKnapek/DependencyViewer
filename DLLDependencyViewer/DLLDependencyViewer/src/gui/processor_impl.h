25de73a2d44ca0d9a85273189e96627fd9d8fef0
Marek Knápek <knapek.mar@gmail.com>
2019-09-13 20:12:00 +0000

    Fix warning C26495.
    Variable 'processor_impl::m_file_name' is uninitialized. Always initialize a member variable (type.6).

--- DLLDependencyViewer/DLLDependencyViewer/src/gui/processor_impl.h
+++ DLLDependencyViewer/DLLDependencyViewer/src/gui/processor_impl.h
@@ -16,7 +16,7 @@ struct processor_impl
 	std::wstring const* m_main_file_path = nullptr;
 	std::queue<file_info*> m_queue;
 	std::unordered_map<string const*, file_info*, string_case_insensitive_hash, string_case_insensitive_equal> m_map;
-	file_name* m_file_name;
+	file_name* m_file_name = nullptr;
 	manifest_parser* m_manifest_parser;
 };
 
