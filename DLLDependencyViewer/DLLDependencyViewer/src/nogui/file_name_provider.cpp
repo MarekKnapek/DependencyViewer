4b5b83cb72853eb0594e36a658a276cfc376ca2a
Marek Knápek <knapek.mar@gmail.com>
2019-10-17 02:05:00 +0000

    Rename file_name_provider to file_name_provider.

---
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/file_name_provider.cpp
@@ -0,0 +1,93 @@
+#include "file_name.h"
+
+#include <cassert>
+
+
+static constexpr wchar_t const s_file_name_err[] = L"Error.";
+
+
+static file_name* g_file_name = nullptr;
+
+
+void file_name::init()
+{
+	assert(!g_file_name);
+	g_file_name = new file_name();
+}
+
+void file_name::deinit()
+{
+	assert(g_file_name);
+	delete g_file_name;
+	g_file_name = nullptr;
+}
+
+wstring const* file_name::get_correct_file_name(wchar_t const* const& file_name, int const& file_name_len, wunique_strings& us, allocator& alc)
+{
+	assert(g_file_name);
+	return g_file_name->get_correct_file_name_(file_name, file_name_len, us, alc);
+}
+
+file_name::file_name()
+{
+	HRESULT const com_inited = OleInitialize(nullptr);
+	if(!(com_inited == S_OK || com_inited == S_FALSE))
+	{
+		throw s_file_name_err;
+	}
+	HRESULT const get_clsid = CLSIDFromProgID(L"Scripting.FileSystemObject", &m_clsid);
+	if(!(get_clsid == S_OK))
+	{
+		throw s_file_name_err;
+	}
+	HRESULT const instance_created = CoCreateInstance(m_clsid, nullptr, CLSCTX_INPROC_SERVER, IID_IDispatch, reinterpret_cast<void**>(&m_object));
+	if(!(instance_created == S_OK))
+	{
+		throw s_file_name_err;
+	}
+	static constexpr wchar_t const s_method_name[] = L"GetAbsolutePathName";
+	wchar_t const* const method_name = s_method_name;
+	HRESULT const got_method = m_object->lpVtbl->GetIDsOfNames(m_object, IID_NULL, const_cast<wchar_t**>(&method_name), 1, LOCALE_USER_DEFAULT, &m_method);
+	if(!(got_method == S_OK))
+	{
+		throw s_file_name_err;
+	}
+}
+
+file_name::~file_name()
+{
+	m_object->lpVtbl->Release(m_object);
+	OleUninitialize();
+}
+
+wstring const* file_name::get_correct_file_name_(wchar_t const* const& file_name, int const& file_name_len, wunique_strings& us, allocator& alc)
+{
+	VARIANT input_1;
+	input_1.vt = VT_BSTR;
+	input_1.bstrVal = SysAllocStringLen(file_name, file_name_len);
+	DISPPARAMS input;
+	input.rgvarg = &input_1;
+	input.rgdispidNamedArgs = nullptr;
+	input.cArgs = 1;
+	input.cNamedArgs = 0;
+	VARIANT output;
+	output.vt = VT_BSTR;
+	HRESULT const invoked = m_object->lpVtbl->Invoke(m_object, m_method, m_clsid, LOCALE_USER_DEFAULT, DISPATCH_METHOD, &input, &output, nullptr, nullptr);
+	if(!(invoked == S_OK))
+	{
+		throw s_file_name_err;
+	}
+	if(!(output.bstrVal))
+	{
+		throw s_file_name_err;
+	}
+	int const output_len = static_cast<int>(*reinterpret_cast<std::uint32_t const*>(reinterpret_cast<char const*>(output.bstrVal) - 4));
+	if(output_len % 2 != 0)
+	{
+		throw s_file_name_err;
+	}
+	wstring const* const ret = us.add_string(output.bstrVal, output_len / 2, alc);
+	SysFreeString(output.bstrVal);
+	SysFreeString(input_1.bstrVal);
+	return ret;
+}
---
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/file_name_provider.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include "allocator.h"
+#include "unique_strings.h"
+
+#include "my_windows.h"
+#include <objbase.h>
+#include <ole2.h>
+
+
+class file_name
+{
+public:
+	static void init();
+	static void deinit();
+	static wstring const* get_correct_file_name(wchar_t const* const& file_name, int const& file_name_len, wunique_strings& us, allocator& alc);
+private:
+	file_name();
+	~file_name();
+	wstring const* get_correct_file_name_(wchar_t const* const& file_name, int const& file_name_len, wunique_strings& us, allocator& alc);
+private:
+	CLSID m_clsid;
+	IDispatch* m_object;
+	DISPID m_method;
+};
