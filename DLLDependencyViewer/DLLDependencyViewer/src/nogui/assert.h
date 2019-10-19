02a6d427d6d51f0ec69b3b442d7a19fa0181e0b0
Marek Knápek <knapek.mar@gmail.com>
2019-10-19 12:01:00 +0000

    Warning will print file name and line number.

--- DLLDependencyViewer/DLLDependencyViewer/src/nogui/assert.h
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/assert.h
@@ -4,8 +4,14 @@
 #include <cassert>
 
 
-#define WARN_M(X, M) do{ if(!(X)){ assert((assert_function(L"DLLDependencyViewer Warning: " M L"\x0D\x0A"), true)); } }while(false)
-#define WARN_M_R(X, M, R) do{ if(!(X)){ assert((assert_function(L"DLLDependencyViewer Warning: " M L"\x0D\x0A"), true)); return R; } }while(false)
+#define WARN_XXX_1(X) L#X
+#define WARN_XXX_2(X) WARN_XXX_1(X)
+#define WARN_XXX_3(X) L##X
+#define WARN_XXX_4(X) WARN_XXX_3(X)
+
+
+#define WARN_M(X, M) do{ if(!(X)){ assert((assert_function(L"DLLDependencyViewer Warning! File: " WARN_XXX_4(__FILE__) L", Line: " WARN_XXX_2(__LINE__) L", Message: " M L"\x0D\x0A"), true)); } }while(false)
+#define WARN_M_R(X, M, R) do{ if(!(X)){ assert((assert_function(L"DLLDependencyViewer Warning! File: " WARN_XXX_4(__FILE__) L", Line: " WARN_XXX_2(__LINE__) L", Message: " M L"\x0D\x0A"), true)); return R; } }while(false)
 
 
 void assert_function(wchar_t const* const& str);
