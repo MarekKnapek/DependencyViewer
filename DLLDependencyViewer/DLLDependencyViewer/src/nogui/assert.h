9402b23f9bfd3f7f06c7531e7898592d8ba0724d
Marek Knápek <knapek.mar@gmail.com>
2019-12-08 02:06:00 +0000

    WARN_M_RV

--- DLLDependencyViewer/DLLDependencyViewer/src/nogui/assert.h
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/assert.h
@@ -12,6 +12,7 @@
 
 #define WARN_M(X, M) do{ if(!(X)) [[unlikely]] { assert((assert_function(L"DLLDependencyViewer Warning! File: " WARN_XXX_4(__FILE__) L", Line: " WARN_XXX_2(__LINE__) L", Message: " M L"\x0D\x0A"), true)); } }while(false)
 #define WARN_M_R(X, M, R) do{ if(!(X)) [[unlikely]] { assert((assert_function(L"DLLDependencyViewer Warning! File: " WARN_XXX_4(__FILE__) L", Line: " WARN_XXX_2(__LINE__) L", Message: " M L"\x0D\x0A"), true)); return R; } }while(false)
+#define WARN_M_RV(X, M) do{ if(!(X)) [[unlikely]] { assert((assert_function(L"DLLDependencyViewer Warning! File: " WARN_XXX_4(__FILE__) L", Line: " WARN_XXX_2(__LINE__) L", Message: " M L"\x0D\x0A"), true)); return; } }while(false)
 
 
 void assert_function(wchar_t const* const& str);
