40c1569010d45c6ece23bf67e259c28b55a42af6
Marek Knápek <knapek.mar@gmail.com>
2019-12-08 02:02:00 +0000

    to_lowercase

--- DLLDependencyViewer/DLLDependencyViewer/src/nogui/unicode.cpp
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/unicode.cpp
@@ -11,3 +11,32 @@ bool is_ascii(char_t const* const str, int const size)
 
 template bool is_ascii<char>(char const* const str, int const size);
 template bool is_ascii<wchar_t>(wchar_t const* const str, int const size);
+
+template<>
+char to_lowercase(char const ch)
+{
+	if(ch >= 'A' && ch <= 'Z')
+	{
+		return 'a' + (ch - 'A');
+	}
+	else
+	{
+		return ch;
+	}
+}
+
+template<>
+wchar_t to_lowercase(wchar_t const ch)
+{
+	if(ch >= L'A' && ch <= L'Z')
+	{
+		return L'a' + (ch - L'A');
+	}
+	else
+	{
+		return ch;
+	}
+}
+
+template char to_lowercase<char>(char const ch);
+template wchar_t to_lowercase<wchar_t>(wchar_t const ch);
--- DLLDependencyViewer/DLLDependencyViewer/src/nogui/unicode.h
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/unicode.h
@@ -3,3 +3,5 @@
 
 template<typename char_t>
 bool is_ascii(char_t const* const str, int const size);
+template<typename char_t>
+char_t to_lowercase(char_t const ch);
