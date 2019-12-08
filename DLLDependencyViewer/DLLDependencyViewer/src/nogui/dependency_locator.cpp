364fc394a89384fb2517f7a52d809b40f15e7566
Marek Knápek <knapek.mar@gmail.com>
2019-12-08 02:13:00 +0000

    locate_dependency_known_dlls

--- DLLDependencyViewer/DLLDependencyViewer/src/nogui/dependency_locator.cpp
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/dependency_locator.cpp
@@ -1,5 +1,10 @@
 #include "dependency_locator.h"
 
+#include "known_dlls.h"
+#include "unicode.h"
+
+#include <filesystem>
+
 
 bool locate_dependency(dependency_locator& self)
 {
@@ -22,7 +27,25 @@ bool locate_dependency_sxs(dependency_locator& self)
 
 bool locate_dependency_known_dlls(dependency_locator& self)
 {
-	return false;
+	string_handle const& dependency = *self.m_dependency;
+	std::string& tmpn = self.m_tmpn;
+	self.m_tmpn.resize(size(dependency));
+	std::transform(begin(dependency), end(dependency), begin(tmpn), [](auto const& e){ return to_lowercase(e); });
+	string const dll_name_s{tmpn.c_str(), static_cast<int>(tmpn.size())};
+	string_handle const dll_name_sh{&dll_name_s};
+	std::vector<std::string> const& known_dll_names = get_known_dll_names_sorted_lowercase_ascii();
+	auto const it = std::lower_bound(known_dll_names.begin(), known_dll_names.end(), dll_name_sh, [](auto const& e, auto const& v){ string const e_s{e.c_str(), static_cast<int>(e.size())}; return string_handle{&e_s} < v; });
+	if(it == known_dll_names.end())
+	{
+		return false;
+	}
+	string const s{it->c_str(), static_cast<int>(it->size())};
+	if(string_handle{&s} != dll_name_sh)
+	{
+		return false;
+	}
+	self.m_result = std::filesystem::path{get_knonw_dlls_path()}.append(*it);
+	return true;
 }
 
 bool locate_dependency_application_dir(dependency_locator& self)
--- DLLDependencyViewer/DLLDependencyViewer/src/nogui/dependency_locator.h
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/dependency_locator.h
@@ -3,10 +3,15 @@
 
 #include "my_string_handle.h"
 
+#include <string>
+
 
 struct dependency_locator
 {
-	wstring_handle m_main_path;
+	wstring_handle const* m_main_path;
+	string_handle const* m_dependency;
+	std::wstring m_result;
+	std::string m_tmpn;
 };
 
 
