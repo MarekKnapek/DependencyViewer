0cf8f95ed34417add4dda7daa79ab060eeb7d313
Marek Knápek <knapek.mar@gmail.com>
2020-01-22 02:04:00 +0000

    Refactor file_name_provider.

--- DLLDependencyViewer/DLLDependencyViewer/src/nogui/file_name_provider.cpp
+++ DLLDependencyViewer/DLLDependencyViewer/src/nogui/file_name_provider.cpp
@@ -1,9 +1,9 @@
 #include "file_name_provider.h"
 
+#include "assert.h"
+#include "scope_exit.h"
+
 #include <cassert>
-
-
-static constexpr wchar_t const s_file_name_err[] = L"Error.";
 
 
 static file_name_provider* g_file_name_provider = nullptr;
@@ -28,60 +28,60 @@ wstring_handle file_name_provider::get_correct_file_name(wchar_t const* const& f
 	return g_file_name_provider->get_correct_file_name_(file_name, file_name_len, us, alc);
 }
 
-file_name_provider::file_name_provider()
+file_name_provider::file_name_provider() :
+	m_clsid(),
+	m_object(),
+	m_method()
 {
-	HRESULT const get_clsid = CLSIDFromProgID(L"Scripting.FileSystemObject", &m_clsid);
-	if(!(get_clsid == S_OK))
-	{
-		throw s_file_name_err;
-	}
-	HRESULT const instance_created = CoCreateInstance(m_clsid, nullptr, CLSCTX_INPROC_SERVER, IID_IDispatch, reinterpret_cast<void**>(&m_object));
-	if(!(instance_created == S_OK))
-	{
-		throw s_file_name_err;
-	}
+	CLSID clsid;
+	HRESULT const get_clsid = CLSIDFromProgID(L"Scripting.FileSystemObject", &clsid);
+	WARN_M_RV(get_clsid == S_OK, L"Failed to CLSIDFromProgID.");
+	IDispatch* object;
+	HRESULT const instance_created = CoCreateInstance(clsid, nullptr, CLSCTX_INPROC_SERVER, IID_IDispatch, reinterpret_cast<void**>(&object));
+	WARN_M_RV(instance_created == S_OK, L"Failed to CoCreateInstance.");
 	static constexpr wchar_t const s_method_name[] = L"GetAbsolutePathName";
 	wchar_t const* const method_name = s_method_name;
-	HRESULT const got_method = m_object->lpVtbl->GetIDsOfNames(m_object, IID_NULL, const_cast<wchar_t**>(&method_name), 1, LOCALE_USER_DEFAULT, &m_method);
-	if(!(got_method == S_OK))
-	{
-		throw s_file_name_err;
-	}
+	DISPID method;
+	HRESULT const got_method = object->lpVtbl->GetIDsOfNames(object, IID_NULL, const_cast<wchar_t**>(&method_name), 1, LOCALE_USER_DEFAULT, &method);
+	WARN_M_RV(got_method == S_OK, L"Failed to IDispatch::GetIDsOfNames.");
+	m_clsid = clsid;
+	m_object = object;
+	m_method = method;
 }
 
 file_name_provider::~file_name_provider()
 {
+	if(!m_object)
+	{
+		return;
+	}
 	m_object->lpVtbl->Release(m_object);
 }
 
 wstring_handle file_name_provider::get_correct_file_name_(wchar_t const* const& file_name, int const& file_name_len, wunique_strings& us, allocator& alc)
 {
-	VARIANT input_1;
-	input_1.vt = VT_BSTR;
-	input_1.bstrVal = SysAllocStringLen(file_name, file_name_len);
+	if(!m_object)
+	{
+		return {};
+	}
+	VARIANT arg_1;
+	arg_1.vt = VT_BSTR;
+	arg_1.bstrVal = SysAllocStringLen(file_name, file_name_len);
+	auto const free_sys_string_1 = mk::make_scope_exit([&](){ SysFreeString(arg_1.bstrVal); });
 	DISPPARAMS input;
-	input.rgvarg = &input_1;
+	input.rgvarg = &arg_1;
 	input.rgdispidNamedArgs = nullptr;
 	input.cArgs = 1;
 	input.cNamedArgs = 0;
 	VARIANT output;
 	output.vt = VT_BSTR;
+	output.bstrVal = nullptr;
 	HRESULT const invoked = m_object->lpVtbl->Invoke(m_object, m_method, m_clsid, LOCALE_USER_DEFAULT, DISPATCH_METHOD, &input, &output, nullptr, nullptr);
-	if(!(invoked == S_OK))
-	{
-		throw s_file_name_err;
-	}
-	if(!(output.bstrVal))
-	{
-		throw s_file_name_err;
-	}
-	int const output_len = static_cast<int>(*reinterpret_cast<std::uint32_t const*>(reinterpret_cast<char const*>(output.bstrVal) - 4));
-	if(output_len % 2 != 0)
-	{
-		throw s_file_name_err;
-	}
+	WARN_M_R(invoked == S_OK, L"Failed to IDispatch::Invoke.", {});
+	WARN_M_R(output.bstrVal, L"Failed to IDispatch::Invoke.", {});
+	auto const free_sys_string_2 = mk::make_scope_exit([&](){ SysFreeString(output.bstrVal); });
+	int const output_len = static_cast<int>(*reinterpret_cast<std::uint32_t const*>(reinterpret_cast<char const*>(output.bstrVal) - sizeof(std::uint32_t)));
+	assert(output_len % 2 == 0);
 	wstring_handle const ret = us.add_string(output.bstrVal, output_len / 2, alc);
-	SysFreeString(output.bstrVal);
-	SysFreeString(input_1.bstrVal);
 	return ret;
 }
