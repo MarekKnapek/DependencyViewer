56f2386d1aa99646283b98302a279b75f8dd5f16
Marek Knápek <knapek.mar@gmail.com>
2020-01-25 02:09:00 +0000

    Add documentation for tree algorithms.

--- DLLDependencyViewer/DLLDependencyViewer/src/gui/tree_algos.cpp
+++ DLLDependencyViewer/DLLDependencyViewer/src/gui/tree_algos.cpp
@@ -5,21 +5,17 @@
 #include <cstdint>
 
 
-void breadth_first_visit(file_info& fi, void(*const callback_fn)(file_info& fi, void* const data), void* const data)
-{
-	std::uint16_t const n = fi.m_import_table.m_dll_count;
-	for(std::uint16_t i = 0; i != n; ++i)
-	{
-		file_info& child_fi = fi.m_fis[i];
-		callback_fn(child_fi, data);
-	}
-	for(std::uint16_t i = 0; i != n; ++i)
-	{
-		file_info& child_fi = fi.m_fis[i];
-		depth_first_visit(child_fi, callback_fn, data);
-	}
-}
-
+/*
+*
+*                                ( - )
+*                  +---------------+---------------+
+*                ( 1 )                           ( 8 )
+*          +-------+-------+               +-------+-------+
+*        ( 2 )           ( 5 )           ( 9 )           ( 12)
+*      +---+---+       +---+---+       +---+---+       +---+---+
+*    ( 3 )   ( 4 )   ( 6 )   ( 7 )   ( 10)   ( 11)   ( 13)   ( 14)
+*
+*/
 void depth_first_visit(file_info& fi, void(*const callback_fn)(file_info& fi, void* const data), void* const data)
 {
 	std::uint16_t const n = fi.m_import_table.m_dll_count;
@@ -31,6 +27,17 @@ void depth_first_visit(file_info& fi, void(*const callback_fn)(file_info& fi, vo
 	}
 }
 
+/*
+*
+*                                ( - )
+*                  +---------------+---------------+
+*                ( 7 )                           ( 14)
+*          +-------+-------+               +-------+-------+
+*        ( 3 )           ( 6 )           ( 10)           ( 13)
+*      +---+---+       +---+---+       +---+---+       +---+---+
+*    ( 1 )   ( 2 )   ( 4 )   ( 5 )   ( 8 )   ( 9 )   ( 11)   ( 12)
+*
+*/
 void children_first_visit(file_info& fi, void(*const callback_fn)(file_info& fi, void* const data), void* const data)
 {
 	std::uint16_t const n = fi.m_import_table.m_dll_count;
--- DLLDependencyViewer/DLLDependencyViewer/src/gui/tree_algos.h
+++ DLLDependencyViewer/DLLDependencyViewer/src/gui/tree_algos.h
@@ -4,6 +4,5 @@
 struct file_info;
 
 
-void breadth_first_visit(file_info& fi, void(*const callback_fn)(file_info& fi, void* const data), void* const data);
 void depth_first_visit(file_info& fi, void(*const callback_fn)(file_info& fi, void* const data), void* const data);
 void children_first_visit(file_info& fi, void(*const callback_fn)(file_info& fi, void* const data), void* const data);
