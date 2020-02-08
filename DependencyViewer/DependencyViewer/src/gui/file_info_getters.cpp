e7be041f02372ff8fb6ac3363b684fb2d1bbef94
Marek Knápek <knapek.mar@gmail.com>
2020-02-08 02:19:00 +0000

    compare_fi_by_path_or_name

--- DependencyViewer/DependencyViewer/src/gui/file_info_getters.cpp
+++ DependencyViewer/DependencyViewer/src/gui/file_info_getters.cpp
@@ -20,3 +20,34 @@ string_handle const& get_dll_name_no_path(file_info const* const& fi)
 	assert(dll_name);
 	return dll_name;
 };
+
+bool compare_fi_by_path_or_name(file_info const* const& a, file_info const* const& b)
+{
+	assert(a);
+	assert(b);
+	assert(!a->m_orig_instance);
+	assert(!b->m_orig_instance);
+	auto const& path_a = a->m_file_path;
+	auto const& path_b = b->m_file_path;
+	if(path_a && path_b)
+	{
+		auto const ret = wstring_handle_case_insensitive_less{}(path_a, path_b);
+		return ret;
+	}
+	else if(!path_a && !path_b)
+	{
+		auto const& dll_name_a = get_dll_name_no_path(a);
+		auto const& dll_name_b = get_dll_name_no_path(b);
+		auto const ret = string_handle_case_insensitive_less{}(dll_name_a, dll_name_b);
+		return ret;
+	}
+	else if(path_a && !path_b)
+	{
+		return false;
+	}
+	else
+	{
+		assert(!path_a && path_b);
+		return true;
+	}
+};
--- DependencyViewer/DependencyViewer/src/gui/file_info_getters.h
+++ DependencyViewer/DependencyViewer/src/gui/file_info_getters.h
@@ -8,3 +8,4 @@ struct file_info;
 
 
 string_handle const& get_dll_name_no_path(file_info const* const& fi);
+bool compare_fi_by_path_or_name(file_info const* const& a, file_info const* const& b);
