60293b20252f32f6e6afdfd422a42fa17e453f76
Marek Knápek <knapek.mar@gmail.com>
2020-02-22 02:14:00 +0000

    import_window_impl::on_wm_contextmenu

--- DependencyViewer/DependencyViewer/src/gui/import_window_impl.h
+++ DependencyViewer/DependencyViewer/src/gui/import_window_impl.h
@@ -40,6 +40,7 @@ private:
 	LRESULT on_message(UINT const& msg, WPARAM const& wparam, LPARAM const& lparam);
 	LRESULT on_wm_size(WPARAM const& wparam, LPARAM const& lparam);
 	LRESULT on_wm_notify(WPARAM const& wparam, LPARAM const& lparam);
+	LRESULT on_wm_contextmenu(WPARAM const& wparam, LPARAM const& lparam);
 	LRESULT on_wm_setfi(WPARAM const& wparam, LPARAM const& lparam);
 	LRESULT on_wm_setundecorate(WPARAM const& wparam, LPARAM const& lparam);
 	LRESULT on_wm_setcmdmatching(WPARAM const& wparam, LPARAM const& lparam);
