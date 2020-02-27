66bc3d2c5a8d4e0c86701866849e6fc11946c1b5
Marek Knápek <knapek.mar@gmail.com>
2020-02-27 02:07:00 +0000

    modules_list_t getters

--- DependencyViewer/DependencyViewer/src/gui/file_info_getters.cpp
+++ DependencyViewer/DependencyViewer/src/gui/file_info_getters.cpp
@@ -3,8 +3,8 @@
 #include "processor.h"
 
 #include "../nogui/cassert_my.h"
-
-#include <cstdint>
+#include "../nogui/string_converter.h"
+#include "../nogui/utils.h"
 
 
 string_handle const& get_dll_name_no_path(file_info const* const& fi)
@@ -20,7 +20,7 @@ string_handle const& get_dll_name_no_path(file_info const* const& fi)
 	auto const& dll_name = parent_fi->m_import_table.m_dll_names[dll_idx];
 	assert(dll_name);
 	return dll_name;
-};
+}
 
 bool compare_fi_by_path_or_name(file_info const* const& a, file_info const* const& b)
 {
@@ -51,4 +51,45 @@ bool compare_fi_by_path_or_name(file_info const* const& a, file_info const* cons
 		assert(!path_a && path_b);
 		return true;
 	}
-};
+}
+
+
+wstring get_modules_list_col_name(modules_list_t const& modlist, std::uint16_t const& idx, string_converter& str_cnvrtr)
+{
+	assert(idx < modlist.m_count);
+	file_info const* const fi = modlist.m_list[idx];
+	assert(!fi->m_orig_instance);
+	if(fi->m_file_path)
+	{
+		wchar_t const* const name = find_file_name(begin(fi->m_file_path), size(fi->m_file_path));
+		assert(name != begin(fi->m_file_path));
+		int const len = size(fi->m_file_path) - static_cast<int>(name - begin(fi->m_file_path));
+		wstring const ret{name, len};
+		return ret;
+	}
+	else
+	{
+		string_handle const& dll_name_a = get_dll_name_no_path(fi);
+		wchar_t const* const dll_name_w = str_cnvrtr.convert(dll_name_a);
+		assert(dll_name_w);
+		wstring const ret{dll_name_w, size(dll_name_a)};
+		return ret;
+	}
+}
+
+wstring get_modules_list_col_path(modules_list_t const& modlist, std::uint16_t const& idx)
+{
+	assert(idx < modlist.m_count);
+	file_info const* const fi = modlist.m_list[idx];
+	assert(!fi->m_orig_instance);
+	if(fi->m_file_path)
+	{
+		wstring const& ret = *fi->m_file_path.m_string;
+		return ret;
+	}
+	else
+	{
+		wstring const ret{L"", 0};
+		return ret;
+	}
+}
