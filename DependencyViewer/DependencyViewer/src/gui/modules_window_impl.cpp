ce3ee71170be18cba80295c122cd1c64bd2ab18b
Marek Knápek <knapek.mar@gmail.com>
2020-02-27 02:03:00 +0000

    Add list view.

--- DependencyViewer/DependencyViewer/src/gui/modules_window_impl.cpp
+++ DependencyViewer/DependencyViewer/src/gui/modules_window_impl.cpp
@@ -1,17 +1,73 @@
 #include "modules_window_impl.h"
 
+#include "common_controls.h"
 #include "main.h"
 
 #include "../nogui/cassert_my.h"
+
+#include <cstdint>
+#include <iterator>
+
+#include "../nogui/windows_my.h"
+
+#include <commctrl.h>
+
+
+enum class e_modules_column___2
+{
+	e_name,
+	e_path,
+};
+static constexpr wchar_t const* const s_modules_headers___2[] =
+{
+	L"name",
+	L"path",
+};
 
 
 ATOM modules_window_impl::g_class;
 
 
 modules_window_impl::modules_window_impl(HWND const& self) :
-	m_self(self)
+	m_self(self),
+	m_list_view()
 {
 	assert(self != nullptr);
+
+	DWORD const ex_style = WS_EX_CLIENTEDGE;
+	wchar_t const* const class_name = WC_LISTVIEWW;
+	wchar_t const* const window_name = nullptr;
+	DWORD const style = (WS_VISIBLE | WS_CHILD | WS_TABSTOP) | (LVS_REPORT | LVS_SHOWSELALWAYS | LVS_OWNERDATA);
+	int const x_pos = 0;
+	int const y_pos = 0;
+	int const width = 0;
+	int const height = 0;
+	HWND const parent = m_self;
+	HMENU const menu = nullptr;
+	HINSTANCE const instance = get_instance();
+	LPVOID const param = nullptr;
+	m_list_view = CreateWindowExW(ex_style, class_name, window_name, style, x_pos, y_pos, width, height, parent, menu, instance, param);
+	assert(m_list_view != nullptr);
+	unsigned const extended_lv_styles = LVS_EX_FULLROWSELECT | LVS_EX_LABELTIP | LVS_EX_DOUBLEBUFFER;
+	LRESULT const prev_style = SendMessageW(m_list_view, LVM_SETEXTENDEDLISTVIEWSTYLE, extended_lv_styles, extended_lv_styles);
+	for(int i = 0; i != static_cast<int>(std::size(s_modules_headers___2)); ++i)
+	{
+		LVCOLUMNW cl;
+		cl.mask = LVCF_FMT | LVCF_WIDTH | LVCF_TEXT | LVCF_SUBITEM;
+		cl.fmt = LVCFMT_LEFT;
+		cl.cx = 200;
+		cl.pszText = const_cast<LPWSTR>(s_modules_headers___2[i]);
+		cl.cchTextMax = 0;
+		cl.iSubItem = i;
+		cl.iImage = 0;
+		cl.iOrder = i;
+		cl.cxMin = 0;
+		cl.cxDefault = 0;
+		cl.cxIdeal = 0;
+		auto const inserted = SendMessageW(m_list_view, LVM_INSERTCOLUMNW, i, reinterpret_cast<LPARAM>(&cl));
+		assert(inserted != -1);
+		assert(inserted == i);
+	}
 }
 
 modules_window_impl::~modules_window_impl()
@@ -116,6 +172,87 @@ LRESULT modules_window_impl::on_wm_destroy(HWND const& hwnd, WPARAM const& wpara
 
 LRESULT modules_window_impl::on_message(UINT const& msg, WPARAM const& wparam, LPARAM const& lparam)
 {
-	LRESULT const ret = DefWindowProcW(m_self, msg, wparam, lparam);
+	switch(msg)
+	{
+		case WM_SIZE:
+		{
+			LRESULT const ret = on_wm_size(wparam, lparam);
+			return ret;
+		}
+		break;
+		case WM_NOTIFY:
+		{
+			LRESULT const ret = on_wm_notify(wparam, lparam);
+			return ret;
+		}
+		break;
+		default:
+		{
+			LRESULT const ret = DefWindowProcW(m_self, msg, wparam, lparam);
+			return ret;
+		}
+		break;
+	}
+}
+
+LRESULT modules_window_impl::on_wm_size(WPARAM const& wparam, LPARAM const& lparam)
+{
+	RECT rect;
+	BOOL const got = GetClientRect(m_self, &rect);
+	assert(got != 0);
+	assert(rect.left == 0 && rect.top == 0);
+	BOOL const moved = MoveWindow(m_list_view, 0, 0, rect.right, rect.bottom, TRUE);
+	assert(moved != 0);
+
+	LRESULT const ret = DefWindowProcW(m_self, WM_SIZE, wparam, lparam);
 	return ret;
+}
+
+LRESULT modules_window_impl::on_wm_notify(WPARAM const& wparam, LPARAM const& lparam)
+{
+	NMHDR& nmhdr = *reinterpret_cast<NMHDR*>(lparam);
+	if(nmhdr.hwndFrom == m_list_view)
+	{
+		switch(nmhdr.code)
+		{
+			case LVN_GETDISPINFOW:
+			{
+				on_getdispinfow(nmhdr);
+			}
+			break;
+		}
+	}
+
+	LRESULT const ret = DefWindowProcW(m_self, WM_NOTIFY, wparam, lparam);
+	return ret;
+}
+
+void modules_window_impl::on_getdispinfow(NMHDR& nmhdr)
+{
+	NMLVDISPINFOW& nm = reinterpret_cast<NMLVDISPINFOW&>(nmhdr);
+	if((nm.item.mask & LVIF_TEXT) != 0)
+	{
+		int const col_ = nm.item.iSubItem;
+		assert(col_ >= static_cast<std::uint16_t>(e_modules_column___2::e_name));
+		assert(col_ <= static_cast<std::uint16_t>(e_modules_column___2::e_path));
+		e_modules_column___2 const col = static_cast<e_modules_column___2>(col_);
+		switch(col)
+		{
+			case e_modules_column___2::e_name:
+			{
+				nm.item.pszText = const_cast<wchar_t*>(L"");
+			}
+			break;
+			case e_modules_column___2::e_path:
+			{
+				nm.item.pszText = const_cast<wchar_t*>(L"");
+			}
+			break;
+			default:
+			{
+				assert(false);
+			}
+			break;
+		}
+	}
 }
